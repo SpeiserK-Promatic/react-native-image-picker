96ad1bd4713434b54a52b6f98a6619d7a25d9057
Levi <levi@yourwebfile.com>
2021-08-07 15:05:31 -0700

    fix uri path

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -269,7 +269,9 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
     else
     {
       // fix android issue where file is deleted if you take a photo then start to take a 2nd photo but cancel - the 1st photo file is deleted
-      imageConfig = new ImageConfig(null, null, 0, 0, 100, 0, false);
+      // imageConfig = new ImageConfig(null, null, 0, 0, 100, 0, false); - this for some reason returned a uri that started with content:// instead of file://
+      imageConfig = imageConfig.withOriginalFile(null);
+      imageConfig = imageConfig.withResizedFile(null);
       
       requestCode = REQUEST_LAUNCH_IMAGE_CAPTURE;
       cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
